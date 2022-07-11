c197b08ce36a371263521e2dab80cd2a92896149
Bill Chung <bill_chungiii@hotmail.com>
2022-07-11 18:38:49 +0800

    加入專案檔案。

---
+++ WpfMultilanguageByCsvSample/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfMultilanguageByCsvSample
+{
+
+   
+    /// <summary>
+    /// App.xaml 的互動邏輯
+    /// </summary>
+    public partial class App : Application
+    {  
+        public static LanguageCode CurrentLanguage { get; private set; }
+
+        public static void SetCurrentLanguage(LanguageCode code)
+        {
+            if (code == LanguageCode.Default)
+            {
+                LanguageProcess.SetLanguage("Default.txt");
+                CurrentLanguage = LanguageCode.Default;
+            }
+            else if (code == LanguageCode.zhtw)
+            {
+                LanguageProcess.SetLanguage("zh-tw.txt");
+                CurrentLanguage = LanguageCode.zhtw;
+            }
+        }       
+
+        public App()
+        {
+            SetCurrentLanguage(LanguageCode.Default);
+            this.Resources.MergedDictionaries.Add(LanguageProcess.Resources);
+        }
+    }
+
+    public enum LanguageCode
+    {
+        Default, zhtw
+    }
+}
